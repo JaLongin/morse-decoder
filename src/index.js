dbc0ba6ce2ac1d43bd0668d076b0512512c7eba1
JaLongin <neznayupochemu1@gmail.com>
2022-07-18 22:37:42 +0300

    feat: implement -morse-decoder

--- src/index.js
+++ src/index.js
@@ -39,6 +39,29 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let symbolsNums = [];
+    for (let i = 0; i < (expr.length / 10); i++){
+        symbolsNums.push(expr.slice(i * 10, (i + 1) * 10 ));
+    }
+    let symbols = new Array(symbolsNums.length);
+    let matRat;
+    symbolsNums.forEach((item, index) => {
+        if (item == '**********'){
+            symbols[index] = ' ';
+        }
+        else {
+            matRat = item.split('');
+            matRat = matRat.reverse();
+            while(matRat.at(-1) == '0')
+                matRat.pop();
+            matRat = matRat.reverse();
+            matRat = matRat.join('');
+            matRat = matRat.replaceAll('11','-');
+            matRat = matRat.replaceAll('10', '.');
+            symbols[index] = MORSE_TABLE[matRat];
+        }
+    });
+    return symbols.join('');;
 }
 
 module.exports = {
